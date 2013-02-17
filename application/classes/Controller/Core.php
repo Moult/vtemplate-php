9cf641ac7463e8da2f033d4c49f27d24f131727b
Dion Moult <dion@thinkmoult.com>
2013-02-17 19:26:49 +1100

    Clean up unnecessary factory code in core

--- application/classes/Controller/Core.php
+++ application/classes/Controller/Core.php
@@ -68,68 +68,6 @@ abstract class Controller_Core extends Controller
             $this->response->body($renderer->render($this->view, $this->template));
         }
         else
-        {
             throw New HTTP_Exception_404('View '.$this->view_class_name.' not found.');
-        }
-    }
-
-    /**
-     * Sends data to a factory to produce a context and executes it
-     *
-     * @param  array $data    The data to send to the factory
-     * @param string $factory The name of the factory to use, if not given, a
-     *                        factory name is guessed
-     * @return array The results of the context execution in the format of:
-     *               array(
-     *                   'status' => 'success' / 'failure',
-     *                   'type' => optional string denoting type of status,
-     *                   'data' => optional array of relevant data,
-     *               )
-     */
-    protected function execute_context($data = NULL, $factory = NULL)
-    {
-        $context = $this->factory($data, $factory)->fetch();
-        $interactor = $context->fetch();
-        return $interactor->execute();
-    }
-
-    /**
-     * Attempts to guess a factory to load either from URI or from param
-     *
-     * @param array  $data    The data to send to the factory
-     * @param string $factory Name of factory to load.
-     * @return Context_Core A "Context_Core" subclass object.
-     */
-    private function factory($data = NULL, $factory = NULL)
-    {
-        if ($factory === NULL)
-            return $this->_factory($data, $this->guess_factory_name_from_uri());
-        else
-            return $this->_factory($data, $factory);
-    }
-
-    /**
-     * Guesses the factory name from the URI.
-     *
-     * @return string
-     */
-    private function guess_factory_name_from_uri()
-    {
-        return 'Factory_'.ucfirst($this->request->controller()).'_'.ucfirst($this->request->action());
-    }
-
-    /**
-     * Loads a factory.
-     *
-     * @param array  $data    The data to send to the factory
-     * @param string $factory Name of the factory class.
-     * @return Factory depending on param $factory
-     */
-    private function _factory($data, $factory)
-    {
-        if ( ! class_exists($factory))
-            throw New HTTP_Exception_404('Factory '.$factory.' not found.');
-
-        return new $factory($data);
     }
 }
