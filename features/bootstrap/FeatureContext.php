39a64c6c0030371c450ba467d2d9c588693824bf
Dion Moult <dion@thinkmoult.com>
2013-07-09 13:17:23 +1000

    Provide a few useful feature definitions
    Features provided:
    
     - Then "element" should [not] be visible
     - Given I have an image with width "" and height "" in ""
     - The "element" element should display "path"
     - The "element" element should be "" by "" pixels

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -30,15 +30,75 @@ class FeatureContext extends Behat\MinkExtension\Context\MinkContext
         // Initialize your context here
     }
 
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
+    /**
+     * @Then /^"([^"]*)" should be visible$/
+     */
+    public function shouldBeVisible($selector)
+    {
+        $element = $this->getSession()->getPage()->find('css', $selector);
+        if (empty($element))
+            throw new Exception('Element "'.$selector.'" not found');
+
+        $display = $this->getSession()->evaluateScript(
+            'jQuery("'.$selector.'").css("display")'
+        );
+
+        if ($display === 'none')
+            throw new Exception('Element "'.$selector.'" is not visible');
+    }
+
+    /**
+     * @Then /^"([^"]*)" should not be visible$/
+     */
+    public function shouldNotBeVisible($selector)
+    {
+        $element = $this->getSession()->getPage()->find('css', $selector);
+        if (empty($element))
+            throw new Exception('Element "'.$selector.'" not found');
+
+        $display = $this->getSession()->evaluateScript(
+            'jQuery("'.$selector.'").css("display")'
+        );
+
+        if ($display !== 'none')
+            throw new Exception('Element "'.$selector.'" is visible');
+    }
+
+    /**
+     * @Given /^I have an image with width "([^"]*)" and height "([^"]*)" in "([^"]*)"$/
+     */
+    public function iHaveAnImageWithWidthAndHeightIn($arg1, $arg2, $arg3)
+    {
+        $image = imagecreate($arg1, $arg2);
+        imagecolorallocate($image, 0, 0, 0);
+        imagepng($image, $arg3);
+    }
+
+    /**
+     * @Given /^the "([^"]*)" element should display "([^"]*)"$/
+     */
+    public function theElementShouldDisplay($selector, $image_path)
+    {
+        $selector_image_path = $this->getSession()->evaluateScript(
+            'jQuery("'.$selector.'").attr("src")'
+        );
+
+        if (md5_file(DOCROOT.substr($selector_image_path, strlen(URL::base()))) !== md5_file($image_path))
+            throw new Exception('Element "'.$selector.'" displays "'.$selector_image_path.'" rather than "'.$image_path.'"');
+    }
+
+    /**
+     * @Given /^the "([^"]*)" element should be "([^"]*)" by "([^"]*)" pixels$/
+     */
+    public function theElementShouldBeByPixels($selector, $width, $height)
+    {
+        $selector_image_path = $this->getSession()->evaluateScript(
+            'jQuery("'.$selector.'").attr("src")'
+        );
+
+        list($selector_width, $selector_height, $type, $attr) = getimagesize(DOCROOT.substr($selector_image_path, strlen(URL::base())));
+
+        if ($width != $selector_width OR $height != $selector_height)
+            throw new Exception('Element "'.$selector.'" is '.$selector_width.'x'.$selector_height.' instead of '.$width.'x'.$height);
+    }
 }
